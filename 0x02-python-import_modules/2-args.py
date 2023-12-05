6e056c134fb8c85283059f0f25bcaa80c334d048
redondo2k <redaothmane2016@gmail.com>
2023-12-05 14:58:44 -0800

    w

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     import sys
     argLen = len(sys.argv)
     if argLen == 1:
-        print("{} arguments.".format(argLen - 1))
+        print("{}arguments.".format(argLen - 1))
     elif argLen == 2:
-        print("{} argument:".format(argLen - 1))
+        print("{}argument:".format(argLen - 1))
     else:
-        print("{} arguments:".format(argLen - 1))
+        print("{}arguments:".format(argLen - 1))
         for i in range(1, argLen):
             print("{}: {}".format(i, sys.argv[i]))
